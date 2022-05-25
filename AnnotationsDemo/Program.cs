eeb4b3ee44bf57ee56eb74215154cbe0db1bd034
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-25 10:48:25 +0530

    Add project files.

---
+++ AnnotationsDemo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Annotations Demo");
+AnnotationsTest.Display();
+Console.ReadKey();
---
+++ ReflectionDemoProject/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReflectionDemoProject;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to reflection Demo");
+CustomerInfo obj = new CustomerInfo();
+ReflectionOnCustomer.ReflectionTest();
+obj.Id = -2;
+Console.WriteLine(obj.Id);
+Console.ReadKey();
